6a5c6684825ad80c87b8a285e2e1f589d1d9be61
Farsit-007 <robayatfarsit@gmail.com>
2024-03-28 03:19:48 +0600

    Mobile Responsive Fixed

--- src/Component/ReadList/Readlist.jsx
+++ src/Component/ReadList/Readlist.jsx
@@ -12,14 +12,14 @@ const Readlist = ({displayData}) => {
             {
                 displayData.map(book => (
                     <div key={book.bookId}>
-                        <div className="card sm:card-side border-2 p-8 md:w-[97%] mt-3 mx-auto bg-base-100">
+                        <div className="card sm:card-side border-2 p-4 md:p-8 md:w-[97%] mt-3 mx-auto bg-base-100">
                             <div className="flex md:w-[35%] justify-center items-center rounded-xl  bg-[#F3F3F3]">
                                 <img src={book.image} width={150} alt="" />
                             </div>
                             <div className="card-body md:w-[65%] p-1 pt-2 pr-5 md:pl-10">
                                 <div className='flex space-y2 flex-col justify-between'>
                                     <div className=''>
-                                        <h2 className="card-title text-3xl playfair font-bold ">{book.bookName}</h2>
+                                        <h2 className="card-title text-2xl md:text-3xl playfair font-bold ">{book.bookName}</h2>
                                         <p className="py-2 workSans font-medium text-[16px]">By : {book.author}</p>
                                     </div>
                                     <div className="flex flex-col md:flex-row items-center gap-10">
@@ -27,10 +27,10 @@ const Readlist = ({displayData}) => {
                                             <div>
                                                 <span className='font-bold'>Tags :</span>
                                             </div>
-                                            <div className="flex gap-3  items-center ">
+                                            <div className="flex gap-1 md:gap-3  items-center ">
                                                 {
                                                     book.tags.map(tag => (
-                                                        <button className="btn workSans bg-[#23BE0A0D] text-[16px] font-medium text-[#23BE0A] rounded-3xl py-0 px-6" key={book.bookId}>
+                                                        <button className="btn workSans bg-[#23BE0A0D] md:text-[16px] font-medium text-[#23BE0A] rounded-3xl py-0 px-6" key={book.bookId}>
                                                             #{tag}
                                                         </button>
                                                     ))
@@ -52,11 +52,11 @@ const Readlist = ({displayData}) => {
                                             <p>Page {book.totalPages}</p>
                                         </div>
                                     </div>
-                                    <div className='pt-2 flex md:gap-5'>
-                                        <button className="btn bg-[#328EFF26] text-[#328EFF]  workSans text-[16px]  rounded-3xl">Category : {book.category}</button>
-                                        <button className="btn rounded-3xl bg-[#FFAC3326] text-[#FFAC33] workSans text-[16px] " >Rating : {book.rating}</button>
+                                    <div className='pt-2 flex  md:gap-5'>
+                                        <button className="btn bg-[#328EFF26] text-[#328EFF]  workSans md:text-[16px]  rounded-3xl">Category : {book.category}</button>
+                                        <button className="btn rounded-3xl bg-[#FFAC3326] text-[#FFAC33] workSans md:text-[16px] " >Rating : {book.rating}</button>
                                         <Link to={`/book/${book.bookId}`}>
-                                            <button className="btn  rounded-3xl workSans font-semibold text-lg  bg-[#23BE0A] text-white">View Details</button>
+                                            <button className="btn  rounded-3xl workSans font-semibold md:text-lg  bg-[#23BE0A] text-white">View Details</button>
                                         </Link>
                                     </div>
                                 </div>
